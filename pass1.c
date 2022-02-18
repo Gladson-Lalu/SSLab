bdc89b115c36f3ef39f683ce6e422efc14ada828
Gladson T Lalu <60311559+Gladson-Lalu@users.noreply.github.com>
2022-02-18 11:36:16 +0530

    UPADATED

--- pass1.c
+++ pass1.c
@@ -4,13 +4,15 @@
 void main() {
 
 	int loc, start, len, found, found1;
-    	char opc[10], opr[10], lb[10], temp[10], mne[10];
-    	FILE *fprg, *fout, *fopt, *fsym;
+    	char opc[10], opr[10], lb[10], temp[10], mne[10],symlb[10],symadr[10];
+    	FILE *fprg, *fout, *fopt, *fsym, *flen, *frsym;
 
     	fprg = fopen("program.txt", "r");
 	fout = fopen("object.txt", "w");
     	fopt = fopen("optab.txt", "r");
     	fsym = fopen("symboltable.txt", "w");
+	frsym = fopen("symboltable.txt", "r");
+	flen = fopen("length.txt", "w");
 	fscanf(fprg, "%s\t%s\t%s", lb, opc, opr);
 	if(strcmp(opc, "START")==0) {
 		start = atoi(opr);
@@ -27,38 +29,43 @@ void main() {
 		if(strcmp(lb, "-")!=0) {
 			fprintf(fsym, "%s\t%d\n", lb, loc);
 		}
-		fscanf(fopt, "%s\t%s", temp, mne);
-		found = 0;
-		while(strcmp(temp, "END")!=0) {
-			if(strcmp(opc, temp)==0) {
-				found = 1;
-				loc+=3;
-				break;
-			}
+			frsym = fopen("symboltable.txt", "r");
+			fseek(frsym,SEEK_SET,0);
 			fscanf(fopt, "%s\t%s", temp, mne);
-		}
-		if(strcmp(opc, "WORD")==0) {
-			loc+=3;
-		}
-		else if(strcmp(opc, "RESW")==0) {
-			loc+=(3*(atoi(opr)));
-		}
-		else if(strcmp(opc, "RESB")==0) {
-			loc+=(atoi(opr));
-		}
-		else if(strcmp(opc, "BYTE")==0) {
-			loc++;
-		}
-		else if(found==0){
-			printf("Undefined Opcode");
-		}
+			found = 0;
+			while(strcmp(temp, "END")!=0) {
+				if(strcmp(opc, temp)==0) {
+					found = 1;
+					loc+=3;
+					break;
+				}
+				fscanf(fopt, "%s\t%s", temp, mne);
+			}
+			if(strcmp(opc, "WORD")==0) {
+				loc+=3;
+			}
+			else if(strcmp(opc, "RESW")==0) {
+				loc+=(3*(atoi(opr)));
+			}
+			else if(strcmp(opc, "RESB")==0) {
+				loc+=(atoi(opr));
+			}
+			else if(strcmp(opc, "BYTE")==0) {
+				loc++;
+			}
+			else if(found==0){
+				printf("Undefined Opcode");
+			}
 
-		fprintf(fout, "%s\t%s\t%s\t\n", lb, opc, opr);
-		fscanf(fprg, "%s\t%s\t%s", lb, opc, opr);
+			fprintf(fout, "%s\t%s\t%s\n", lb, opc, opr);
+			fscanf(fprg, "%s\t%s\t%s", lb, opc, opr);
+		
 	}
 	fprintf(fout, "%d\t%s\t%s\t%s\n", loc, lb, opc, opr);
 	len = loc-start;
 	printf("The length of the program: %d\n", len);
+	fprintf(flen,"%d",len);
+	fclose(flen);
 	fclose(fprg);
 	fclose(fout);
 	fclose(fopt);
--- pass2.c
+++ pass2.c
@@ -1,74 +1,65 @@
-
 #include<stdio.h>
 #include<string.h>
 void main()
 {
-FILE *fint,*ftab,*flen,*fsym,*fout;
-int op1[10],txtlen,txtlen1,i,j=0,len;
-char add[5],symadd[5],op[5],start[10],temp[30],line[20],label[20],mne[10],operand[10],symtab[10],opmne[10];
-fint=fopen("object.txt","r");
-flen=fopen("length.txt","r");
-ftab=fopen("optab.txt","r");
-fsym=fopen("symtab.txt","r");
-fout=fopen("output.txt","w");
-fscanf(fint,"%s%s%s%s",add,label,mne,operand);
-if(strcmp(mne,"START")==0)
-{
-strcpy(start,operand);
-fscanf(flen,"%d",&len);
-}
-printf("H^%s^%s^%d\nT^00%s^",label,start,len,start);
-fprintf(fout,"H^%s^%s^%d\nT^00%s^",label,start,len,start);
-fscanf(fint,"%s%s%s%s",add,label,mne,operand);
-while(strcmp(mne,"END")!=0)
-{
-while(!feof(ftab))
-{
-fscanf(ftab,"%s%s",opmne,op);
-if(strcmp(mne,opmne)==0)
-{
-fclose(ftab);
-while(!feof(fsym))
-{
-fscanf(fsym,"%s%s",symadd,symtab);
-if(strcmp(operand,symtab)==0)
-{
-printf("%s%s^",op,symadd);
-fprintf(fout,"%s%s^",op,symadd);
-break;
-}
-}
-break;
-}
-}
-if((strcmp(mne,"BYTE")==0)||(strcmp(mne,"WORD")==0))
-{
-if(strcmp(mne,"WORD")==0)
-{printf("0000%s^",operand);
-fprintf(fout,"0000%s^",operand);}
-else
-{
-len=strlen(operand);
-for(i=2;i<len;i++)
-{
-printf("%d",operand[i]);
-fprintf(fout,"%d",operand[i]);
-}
-printf("^");
-fprintf(fout,"^");
-}
-}
-fscanf(fint,"%s%s%s%s",add,label,mne,operand);
-ftab=fopen("optab.txt","r");
-fseek(ftab,SEEK_SET,0);
-}
-printf("\nE^00%s",start);
-fprintf(fout,"\nE^00%s",start);
-fclose(fint);
-fclose(ftab);
-fclose(fsym);
-fclose(flen);
-fclose(fout);
+	FILE *fin,*ftab,*flen,*fsym,*fout;
+	int i,j=0,len;
+	char adr[10],symadr[10],op[10],start[10],temp[30],line[20],label[20],opc[10],operand[10],symtab[10],opmne[10];
+	fin=fopen("object.txt","r");
+	flen=fopen("length.txt","r");
+	ftab=fopen("optab.txt","r");
+	fsym=fopen("symboltable.txt","r");
+	fout=fopen("output.txt","w");
+	fscanf(fin,"%s\t%s\t%s\t%s",adr,label,opc,operand);
+	if(strcmp(opc,"START")==0){
+		strcpy(start,operand);
+		fscanf(flen,"%d",&len);
+	}
+	printf("H^%s^%s^%d\nT^00%s^",label,start,len,start);
+	fprintf(fout,"H^%s^%s^%d\nT^00%s^",label,start,len,start);
+	fscanf(fin,"%s\t%s\t%s\t%s",adr,label,opc,operand);
+	while(strcmp(opc,"END")!=0){
+		while(!feof(ftab)){
+			fscanf(ftab,"%s\t%s",opmne,op);
+			if(strcmp(opc,opmne)==0){
+				fclose(ftab);
+				while(!feof(fsym)){
+					fscanf(fsym,"%s\t%s",symadr,symtab);
+					if(strcmp(operand,symtab)==0){
+						printf("%s%s^",op,symadr);
+						fprintf(fout,"%s%s^",op,symadr);
+						break;
+					}
+				}
+				break;
+			}
+		}
+		if((strcmp(opc,"BYTE")==0)||(strcmp(opc,"WORD")==0)){
+			if(strcmp(opc,"WORD")==0){
+				printf("0000%s^",operand);
+				fprintf(fout,"0000%s^",operand);
+			}
+			else{
+				len=strlen(operand);
+				for(i=2;i<len;i++){
+					printf("%d",operand[i]);
+					fprintf(fout,"%d",operand[i]);
+				}
+				printf("^");
+				fprintf(fout,"^");
+			}
+		}
+		fscanf(fin,"%s\t%s\t%s\t%s",adr,label,opc,operand);
+		ftab=fopen("optab.txt","r");
+		fseek(ftab,SEEK_SET,0);
+	}
+	printf("\nE^00%s\n\n",start);
+	fprintf(fout,"\nE^00%s\n\n",start);
+	fclose(fin);
+	fclose(ftab);
+	fclose(fsym);
+	fclose(flen);
+	fclose(fout);
 }
 
 
