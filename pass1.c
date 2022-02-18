8e7ba83484108e2f85c5c6936af0e5848203a2a0
Gladson T Lalu <60311559+Gladson-Lalu@users.noreply.github.com>
2022-02-18 12:16:30 +0530

    updated

--- pass1.c
+++ pass1.c
@@ -4,14 +4,13 @@
 void main() {
 
 	int loc, start, len, found, found1;
-    	char opc[10], opr[10], lb[10], temp[10], mne[10],symlb[10],symadr[10];
+    	char opc[10], opr[10], lb[10], temp[10], mne[10],symlb[20],symadr[20];
     	FILE *fprg, *fout, *fopt, *fsym, *flen, *frsym;
 
     	fprg = fopen("program.txt", "r");
 	fout = fopen("object.txt", "w");
     	fopt = fopen("optab.txt", "r");
-    	fsym = fopen("symboltable.txt", "w");
-	frsym = fopen("symboltable.txt", "r");
+    	fsym = fopen("symboltable.txt", "w+");
 	flen = fopen("length.txt", "w");
 	fscanf(fprg, "%s\t%s\t%s", lb, opc, opr);
 	if(strcmp(opc, "START")==0) {
@@ -28,9 +27,17 @@ void main() {
 		printf("%s\t%s\t%s\n", lb, opc, opr);
 		if(strcmp(lb, "-")!=0) {
 			fprintf(fsym, "%s\t%d\n", lb, loc);
-		}
-			frsym = fopen("symboltable.txt", "r");
-			fseek(frsym,SEEK_SET,0);
+		}else{
+			fseek(fsym,SEEK_SET,0);
+			fscanf(fsym, "%s\t%s",symlb, symadr);
+			while(!feof(fsym)){
+				printf("%s \t %s \n",symlb,symadr);
+				if(strcmp(symlb,lb)==0){
+					printf("Label duplication error");
+					exit(0);
+				}
+				fscanf(fsym,"%s\t%s",symlb,symadr);
+			}
 			fscanf(fopt, "%s\t%s", temp, mne);
 			found = 0;
 			while(strcmp(temp, "END")!=0) {
@@ -56,10 +63,9 @@ void main() {
 			else if(found==0){
 				printf("Undefined Opcode");
 			}
-
-			fprintf(fout, "%s\t%s\t%s\n", lb, opc, opr);
-			fscanf(fprg, "%s\t%s\t%s", lb, opc, opr);
-		
+				
+		}
+		fscanf(fprg, "%s\t%s\t%s", lb, opc, opr);
 	}
 	fprintf(fout, "%d\t%s\t%s\t%s\n", loc, lb, opc, opr);
 	len = loc-start;
